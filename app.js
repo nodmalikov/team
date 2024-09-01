dd5995c55213fda9dd765388478cf669445c0479
nodmalikov <abcmalikov@gmail.com>
2024-09-01 16:02:17 +0500

    Add directors section and make customize

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const elHamburgerMenu = document.querySelector('.js-hamburger-menu');
 const elCloseIcon = document.querySelector('.js-close-icon');
 const elHeaderInner = document.querySelector('.site-header__inner');
 
+const elsCardIcon = document.querySelectorAll('.card-directors__icon');
+const elCardAfter = document.querySelectorAll('.card-directors__after');
+const elCardSvg = document.querySelectorAll('.js-card-svg');
+
 // MOBILE-OPEN-MENU
 if (elHamburgerMenu) {
     elHamburgerMenu.addEventListener('click', function () {
@@ -14,3 +18,11 @@ if (elCloseIcon) {
         elHeaderInner.classList.remove('open-menu');
     });
 }
+
+// CARD-TAP
+elsCardIcon.forEach((elCardIcon, index) => {
+    elCardIcon.addEventListener('click', function () {
+        elCardAfter[index].classList.toggle('directors-card-opacity');
+        elCardSvg[index].classList.toggle('directors-icon-rotate');
+    })
+})
